b59581d8baaeb4bd84ce2a490c68ce74e2d48667
t638403 <e.j.vd.hooven@gmail.com>
2014-10-12 13:22:42 +0200

    Added basic stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+#Intro
+A tool for generating random data
+
+#API
+
+##nonsense.word(min, max)
+Generates a string of random lowercase letters with at least min letters and at most max letters. min defaults to 3 and
+max defaults to 8
--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-/**
- * Created by erik on 10/12/14.
- */
+var nonsense = {
+    /**
+     * Returns a random integer between min and max, inclusive. If you only pass one argument, it will return a number
+     * between 0 and that number.
+     * Copied from underscore.js 1.7.0, http://underscorejs.org/ to prevent a dependency.
+     */
+    random:function(min, max) {
+        if (max == null) {
+            max = min;
+            min = 0;
+        }
+        return min + Math.floor(Math.random() * (max - min + 1));
+    },
+
+    /**
+     * Returns a random sequence of letters with a length between min and max. If you only pass one argument, it will
+     * return a random string of a length between 1 and that number.
+     */
+    word:function(min, max) {
+        if (max == null) {
+            max = min;
+            min = 1;
+        }
+        var wordLength = nonsense.random(min, max);
+        var charCodes = [];
+        for(var i = 0; i < wordLength; i++) {
+            charCodes.push(nonsense.random(97, 122));
+        }
+        return String.fromCharCode.apply(String.fromCharCode, charCodes);
+    }
+}
+
+module.exports = nonsense;
